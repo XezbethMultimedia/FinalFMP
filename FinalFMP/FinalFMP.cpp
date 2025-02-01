33c6bc063f3d1f9e4dc554890fee997faa8d32e7
Lachlan Moreton <99296364@students.camre.ac.uk>
2025-02-01 21:24:14 +0000

    fix pt2

--- FinalFMP/FinalFMP.cpp
+++ FinalFMP/FinalFMP.cpp
@@ -6,67 +6,73 @@ namespace core {
 
 	using json = nlohmann::json;
 
-	class object {
-	public:
-		std::string name = "object";
-		int pos_x = 0;
-		int pos_y = 0;
-		int siz_x = 0;
-		int siz_y = 0;
-	};
+	namespace object {
 
-	void to_json(json& j, const object& o) {
-		j = json{
-			{"name", o.name},
-			{"pos_x", o.pos_x},
-			{"pos_y", o.pos_y},
-			{"siz_x", o.siz_x},
-			{"siz_y", o.siz_y}
+		class object {
+		public:
+			std::string name = "object";
+			int pos_x = 0;
+			int pos_y = 0;
+			int siz_x = 0;
+			int siz_y = 0;
+		};
+
+		void to_json(json& j, const object& o) {
+			j = json{
+				{"name", o.name},
+				{"pos_x", o.pos_x},
+				{"pos_y", o.pos_y},
+				{"siz_x", o.siz_x},
+				{"siz_y", o.siz_y}
+			};
+		}
+
+		void from_json(const json& j, object& o) {
+			j.at("name").get_to(o.name);
+			j.at("pos_x").get_to(o.pos_x);
+			j.at("pos_y").get_to(o.pos_y);
+			j.at("siz_x").get_to(o.siz_x);
+			j.at("siz_y").get_to(o.siz_y);
+		}
+	}
+
+	namespace engine {
+
+		class engine {
+		private:
+			object::object* pObject = NULL;
+			int size = 0;
+		public:
+			void lcObject() {
+				size = 0;
+
+				std::ifstream file("cObject.json");
+				json jObject = json::parse(file);
+
+				for (json::iterator i = jObject.begin(); i != jObject.end(); i++) {
+					size++;
+				}
+
+				pObject = new object::object[size];
+				size = 0;
+
+				for (json::iterator i = jObject.begin(); i != jObject.end(); i++) {
+					*(pObject + size) = i.value().template get<object::object>();
+					std::cout << i.key() << " loaded: " << i.value() << "\n";
+					size += 1;
+				}
+			}
+
+			void clear() {
+				delete[] pObject;
+				std::cout << "Cleard up!\n";
+			}
 		};
 	}
-
-	void from_json(const json& j, object& o) {
-		j.at("name").get_to(o.name);
-		j.at("pos_x").get_to(o.pos_x);
-		j.at("pos_y").get_to(o.pos_y);
-		j.at("siz_x").get_to(o.siz_x);
-		j.at("siz_y").get_to(o.siz_y);
-	}
-
-	class engine {
-	private:
-		object* pObject = NULL;
-		int size = 0;
-	public:
-		void lcObject() {
-			size = 0;
-
-			std::ifstream file("cObject.json");
-			json jObject = json::parse(file);
-
-			for (json::iterator i = jObject.begin(); i != jObject.end(); i++) {
-				size++;
-			}
-
-			pObject = new object[size];
-			size = 0;
-
-			for (json::iterator i = jObject.begin(); i != jObject.end(); i++) {
-				*(pObject + size) = i.value().template get<object>();
-				std::cout << i.key() << " loaded: " << i.value() << "\n";
-				size += 1;
-			}
-		}
-
-		void clear() {
-			delete[] pObject;
-			std::cout << "Cleard up!\n";
-		}
-	};
 }
 
 int main() {
-	core::engine engine;
+	core::engine::engine engine;
 	engine.lcObject();
 	engine.clear();
 	return 0;
