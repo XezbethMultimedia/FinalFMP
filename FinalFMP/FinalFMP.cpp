2319def39ce70b41d3c1d20ab999662c2f1547b0
Lachlan Moreton <99296364@students.camre.ac.uk>
2025-02-03 11:58:02 +0000

    added stuff

--- FinalFMP/FinalFMP.cpp
+++ FinalFMP/FinalFMP.cpp
@@ -36,13 +36,94 @@ namespace core {
 		}
 	}
 
+	namespace entity {
+
+		class entity : public object::object {
+		public:
+			int hp = 0;
+			int xp = 0;
+			int lv = 0;
+
+			int atk = 0;
+			int def = 0;
+			int dmg = 0;
+
+			void death();
+			void lvlup();
+		};
+
+		void to_json(json& j, const entity& e) {
+			j = json{
+				{"name", e.name},
+				{"pos_x", e.pos_x},
+				{"pos_y", e.pos_y},
+				{"siz_x", e.siz_x},
+				{"siz_y", e.siz_y},
+				{"hp", e.hp},
+				{"xp", e.xp},
+				{"lv", e.lv},
+				{"atk", e.atk},
+				{"def", e.def},
+				{"dmg", e.dmg},
+			};
+		}
+
+		void from_json(const json& j, entity& e) {
+			j.at("name").get_to(e.name);
+			j.at("pos_x").get_to(e.pos_x);
+			j.at("pos_y").get_to(e.pos_y);
+			j.at("siz_x").get_to(e.siz_x);
+			j.at("siz_y").get_to(e.siz_y);
+
+			j.at("hp").get_to(e.hp);
+			j.at("xp").get_to(e.xp);
+			j.at("lv").get_to(e.lv);
+
+			j.at("atk").get_to(e.atk);
+			j.at("def").get_to(e.def);
+			j.at("dmg").get_to(e.dmg);
+		}
+	}
+
+	namespace item {
+
+		class item : public object::object {
+		public:
+			int* stat = NULL;
+			int value = 0;
+		};
+
+		void to_json(json& j, const item& i) {
+			j = json{
+				{"name", i.name},
+				{"pos_x", i.pos_x},
+				{"pos_y", i.pos_y},
+				{"siz_x", i.siz_x},
+				{"siz_y", i.siz_y},
+				{"stat", *i.stat},
+				{"value", i.value}
+			};
+		}
+
+		void from_json(const json& j, item& i) {
+			j.at("name").get_to(i.name);
+			j.at("pos_x").get_to(i.pos_x);
+			j.at("pos_y").get_to(i.pos_y);
+			j.at("siz_x").get_to(i.siz_x);
+			j.at("siz_y").get_to(i.siz_y);
+			j.at("stat").get_to(*i.stat);
+			j.at("value").get_to(i.value);
+		}
+	}
+
 	namespace engine {
 
-		class engine {
-		private:
 			object::object* pObject = NULL;
+			entity::entity* pEntity = NULL;
+			item::item* pItem = NULL;
+
 			int size = 0;
-		public:
+
 			void lcObject() {
 				size = 0;
 
@@ -59,21 +140,66 @@ namespace core {
 				for (json::iterator i = jObject.begin(); i != jObject.end(); i++) {
 					*(pObject + size) = i.value().template get<object::object>();
 					std::cout << i.key() << " loaded: " << i.value() << "\n";
-					size += 1;
+					size ++;
+				}
+			}
+
+			void lcEntity() {
+				size = 0;
+
+				std::ifstream file("cEntity.json");
+				json jEntity = json::parse(file);
+
+				for (json::iterator i = jEntity.begin(); i != jEntity.end(); i++) {
+					size++;
+				}
+
+				pEntity = new entity::entity[size];
+				size = 0;
+
+				for (json::iterator i = jEntity.begin(); i != jEntity.end(); i++) {
+					*(pEntity + size) = i.value().template get<entity::entity>();
+					std::cout << i.key() << " loaded: " << i.value() << "\n";
+					size++;
+				}
+			}
+
+			void lcItem() {
+				size = 0;
+
+				std::ifstream file("cItem.json");
+				json jItem = json::parse(file);
+
+				for (json::iterator i = jItem.begin(); i != jItem.end(); i++) {
+					size++;
+				}
+
+				pItem = new item::item[size];
+				size = 0;
+
+				for (json::iterator i = jItem.begin(); i != jItem.end(); i++) {
+					*(pItem + size) = i.value().template get<item::item>();
+					std::cout << i.key() << " loaded: " << i.value() << "\n";
+					size++;
 				}
 			}
 
 			void clear() {
 				delete[] pObject;
-				std::cout << "Cleard up!\n";
+				delete[] pEntity;
+				delete[] pItem;
+				std::cout << "Cleared up!\n";
 			}
-		};
 	}
 }
 
 int main() {
-	core::engine::engine engine;
-	engine.lcObject();
-	engine.clear();
+	core::engine::lcEntity();
+
+	int& stat = core::engine::pEntity->atk;
+	std::cout << stat << "\n";
+
+	core::engine::lcItem();
+	core::engine::clear();
 	return 0;
 }
